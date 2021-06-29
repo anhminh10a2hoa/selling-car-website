50d88acd64c4ad88bbfbe30a1a66c09fc5aca669
Minh Hoang <anhminh10a2hoa@gmail.com>
2021-06-30 00:16:15 +0300

    add topCars and footer component

--- front-end/src/app/components/logo/index.tsx
+++ front-end/src/app/components/logo/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
-import CarLogoImg from '../../../assets/images/car-logo.png'
+import CarLogoImg from "../../../assets/images/car-logo.png";
+import CarLogoDarkImg from "../../../assets/images/car-logo-dark.png";
+
+interface ILogoProps {
+  color?: "white" | "dark";
+  bgColor?: "white" | "dark";
+}
 
 const LogoContainer = styled.div`
   ${tw`
     flex
     items-center
-  `}
-`
+  `};
+`;
 
 const LogoText = styled.div`
   ${tw`
@@ -18,27 +24,28 @@ const LogoText = styled.div`
     font-bold
     text-black
     m-1
-  `}
-`
+    `};
+  ${({ color }: any) => (color === "white" ? tw`text-white` : tw`text-black`)}
+` as any;
 
 const Image = styled.div`
   width: auto;
-  ${tw`
-    h-6
-    md:h-9
-  `}
-
+  ${tw`h-6 md:h-9`};
   img {
     width: auto;
     height: 100%;
   }
-`
+`;
 
-export function Logo() {
-  return <LogoContainer>
-    <Image>
-      <img src={CarLogoImg} alt='logo'/>
-    </Image>
-    <LogoText>AMcar</LogoText>
-  </LogoContainer>
+export function Logo(props: ILogoProps) {
+  const { color, bgColor } = props;
+
+  return (
+    <LogoContainer>
+      <Image>
+        <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg} />
+      </Image>
+      <LogoText color={color || "dark"}>Yourcar.</LogoText>
+    </LogoContainer>
+  );
 }
--- front-end/src/app/containers/HomePage/index.tsx
+++ front-end/src/app/containers/HomePage/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { BookCard } from "../../components/bookCard";
+import { Footer } from "../../components/footer";
 import { Marginer } from "../../components/marginer";
-import { BookCard } from "../../components/bookCard/index";
-import { BookingSteps } from "./bookingSteps";
+import { Navbar } from "../../components/navbar";
 import { AboutUs } from "./aboutUs";
-import { Navbar } from "../../components/navbar/index"
+import { BookingSteps } from "./bookingSteps";
+import { TopCars } from "./topCars";
 import { TopSection } from "./topSection";
 
 const PageContainer = styled.div`
@@ -30,6 +32,9 @@ export function HomePage() {
       <BookingSteps />
       <Marginer direction="vertical" margin="8em" />
       <AboutUs />
+      <Marginer direction="vertical" margin="8em" />
+      <TopCars />
+      <Footer />
     </PageContainer>
-  )
+  );
 }
